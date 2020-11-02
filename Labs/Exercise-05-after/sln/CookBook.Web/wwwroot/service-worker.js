c906b5ee80e593a6446966eaee444fa7bf03ca66
Roman Jašek <roman.jasek@riganti.cz>
2020-11-02 16:47:34 +0100

    Added code and presentation for Exercise-05.

---
+++ Labs/Exercise-05-after/sln/CookBook.Web/wwwroot/service-worker.js
@@ -0,0 +1,29 @@
+﻿const CACHE = "precache";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+workbox.precaching.precacheAndRoute([
+    { url: '/sample-data/ingredients.json', revision: '20201027' },
+    { url: '/sample-data/recipes.json', revision: '20201027' },
+    { url: '/scripts/online-status.js', revision: '20201027' }
+]);
+
+workbox.routing.registerRoute(
+    new RegExp('https://localhost:44355/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.NetworkFirst({
+        cacheName: CACHE
+    })
+);
